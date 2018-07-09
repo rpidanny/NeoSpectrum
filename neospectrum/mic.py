a777ccf41172297168b81cbf9d87dd8f963b76d9
rpidanny <abhishekmaharjan1993@gmail.com>
2018-07-09 15:02:51 +0200

    skeleton classes for fft and mic

--- neospectrum/mic.py
+++ neospectrum/mic.py
@@ -1,3 +1,27 @@
 '''
   Microphone class to handle all audio related tasks.
 '''
+import pyaudio
+import numpy as np
+
+
+class Mic:
+    FORMAT = pyaudio.paFloat32
+    CHANNELS = 1
+
+    def __init__(self, rate, bins, chunk=512):
+        self.rate = rate
+        self.chunk = chunk
+        self.bins = bins
+        self.pa = pyaudio.PyAudio()
+        self.stream = self.pa.open(format=self.FORMAT,
+                                   channels=self.CHANNELS,
+                                   rate=self.rate,
+                                   input=True,
+                                   output=False,
+                                   frames_per_buffer=self.chunk)
+
+    def getchunk(self):
+        ret = self.stream.read(self.chunk)
+        ret = np.fromstring(ret, np.float32)
+        return ret
