0b29d1532b1490426dfa003913c7a14c5005969f
rpidanny <abhishekmaharjan1993@gmail.com>
2018-07-10 14:17:15 +0200

    Neo connection on new thread for nonblocking

--- neospectrum/neo.py
+++ neospectrum/neo.py
@@ -1,17 +1,22 @@
 '''
   Neo class to handle all Matrix display communication using Neo library.
 '''
+import thread
 from websocket import create_connection
 
 class Neo:
-
+  ws = None
   def __init__(self, host):
     self.host = host
     # print("ws://{}:{}".format(ip, port))
-    self.ws = create_connection("ws://{}".format(host))
+    thread.start_new_thread(self.connect, ())
+
+  def connect(self):
+    self.ws = create_connection("ws://{}".format(self.host))
 
   def update(self, frame):
-    self.ws.send(frame)
+    if self.ws != None:
+      self.ws.send(frame)
 
   def encode(self, data):
     # TODO: implement encoding spectrum to Neo compatible datastructure
