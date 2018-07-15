2cfcdf7a67a5c9a2e86c2e66d5d01a4f22ee4896
rpidanny <abhishekmaharjan1993@gmail.com>
2018-07-15 11:52:27 +0200

    added daemon mode

--- neospectrum.py
+++ neospectrum.py
@@ -18,6 +18,8 @@ def options():
       description="Audio Spectrum Visualization with Neo")
   parser.add_argument(
       "-d", "--display", help="IP Address of Neo Display", required=True)
+  parser.add_argument(
+      "-D", "--daemon", help="Daemon mode without Plot", action="store_true")
   args = parser.parse_args()
   return args
 
@@ -36,16 +38,25 @@ def main():
   args = options()
 
   mic = Mic(SAMPLE, BINS)
-  plt = Plot(SAMPLE, BINS)
 
   # Neo listens on port 81
   neo = Neo("{}:81".format(args.display))
 
-  # setup animation loop
-  ani = animation.FuncAnimation(plt.fig, animate, fargs=(
-      plt.line, mic, neo), blit=True, interval=1000.0 / FPS, repeat=False)
-  plt.show()
-  mic.disconnect()
+  if args.daemon:
+    print "Press ctrl + c to quit"
+    try:
+      while True:
+        wave_x, wave_y, spec_x, spec_y = mic.getchunk()
+        neo.update(spec_y)
+    except KeyboardInterrupt:
+      mic.disconnect()
+  else:
+    plt = Plot(SAMPLE, BINS)
+    # setup animation loop
+    ani = animation.FuncAnimation(plt.fig, animate, fargs=(
+        plt.line, mic, neo), blit=True, interval=1000.0 / FPS, repeat=False)
+    plt.show()
+    mic.disconnect()
   print "Good Bye!"
 
 if __name__ == "__main__":
